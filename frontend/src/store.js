56e7d9578c211f7f03982482dd5747c82ae3a1fd
dvirkap <37699331+dvirkap@users.noreply.github.com>
2019-03-20 19:17:37 +0200

    posts list working

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -7,14 +7,26 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    posts: []
   },
   mutations: {
+    postsFiltered(state, posts) {
+      state.posts = posts
+    }
+  },
 
+  getters: {
+    postsFiltered(state) {
+      return state.posts
+    }
   },
   actions: {
     loadUser(context) {
       return userService.get
+    },
+    async loadPosts(context, payload){
+     let posts = await PostService.query()
+      context.commit('postsFiltered', posts)
     }
   }
 })
